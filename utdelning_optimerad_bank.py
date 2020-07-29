0e92522a36c2d2a69b04322591c32e917d9cc35d
ZachariasHultman <zacharias.hultman@gmail.com>
2020-07-29 21:48:24 +0200

    workx
    algorithm workx. More to be added to increase time

--- utdelning_optimerad_bank.py
+++ utdelning_optimerad_bank.py
@@ -1,6 +1,9 @@
 #För att hitta upprepningar funkar inte så bra just nu
 #axfood läggs inte till i max_dev_hist. Varför???
 #om magic number > 1 blir det för dyrt????
+#funkar inte om man byter ordning i listan i excell. Varför i fan????
+
+
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jun  18 17:44:14 2020
@@ -13,85 +16,149 @@ from pandas import ExcelWriter
 import networkx as nx
 import matplotlib.pyplot as plt
 import math
+import sys
 
-
-def build_graph(start, bank, excell_data, graph, best_stock):
+def build_graph(start, bank, excell_data, graph, best_stock,best_stock_tmp):
     global max_dev
-    global hidden_bank
     global magic_number
     global cheapest_stock
     global best_stock_history
     global best_stock_history_tmp
-    global best_stock_tmp
-
+    global bank_init
     global history
     global max_dev_hist
+    global flagsator
+
     for stock in excell_data.index:
         flagger=False
+        dev = 0
         # reset the best_stock when a new path from origin is started
         if start == 0:
+            bank=bank_init
             best_stock = []
+            best_stock_tmp=[]
+#            prev_stock=0
+          
+        # calc the cost
+        cost = stock_rates.iloc[stock].to_numpy()
+        # buy the stock
+        bank_tmp = bank-cost
+        
        # check if you've already have bought the max amount if this stock
-        if len(list(filter(lambda i: i['name'] == excell_data.iloc[stock, 0] , best_stock))) < magic_number:
-            dev = 0
+        # did you afford it? if not continue to the next stock and try again
+        if len(list(filter(lambda i: i['name'] == excell_data.iloc[stock, 0] , best_stock))) < magic_number and bank_tmp >= 0:
+#                         
+            best_stock.append({'name':excell_data.iloc[stock, 0],'index':stock})
+            best_stock=sorted(best_stock, key=lambda i: i['index'])
+              
+            best_stock_tmp.append({'name':excell_data.iloc[stock, 0],'index':stock,'bank':bank,'bank_tmp':bank_tmp})
+            best_stock_tmp=sorted(best_stock_tmp, key=lambda i: i['index'])
+
+#                # if you have made a combination that you've already have done before, continiue to next stock
+            for hist in history:
+                flag= hist==best_stock
+                if flag==True:
+                    flagger=flag
+                    break
+            if flagger==True:
+                best_stock=del_first_occurence(best_stock,stock)
+                best_stock_tmp=del_first_occurence(best_stock_tmp,stock)
+                continue
+            
             for month in range(0, len(calendar.columns)):
                 # acess the devidend in month for every stock
-                dev = dev+calendar.iloc[stock, month]
+                dev = dev+calendar.iloc[stock, month]   
             # calculate the total yearly devidend
             dev_tot = np.round(start+dev)
-            bank_tmp = bank
-            # calc the cost
-            cost = stock_rates.iloc[stock].to_numpy()
-            # buy the stock
-            bank_tmp = bank_tmp-cost
-            # did you afford it? if not continue to the next stock and try again
-            if bank_tmp >= 0: 
-                best_stock.append({'name':excell_data.iloc[stock, 0],'index':stock})
-                best_stock=sorted(best_stock, key=lambda i: i['index'])
-#                best_stock_tmp.append({'name':excell_data.iloc[stock, 0],'index':stock,'bank':bank})
-#                # if you have made a combination that you've already have done before, continiue to next stock
-                if len(best_stock)>1:
-                    for hist in history:
-                        flag= hist==best_stock
-                        if flag==True:
-                            flagger=flag
-                            break
-                if flagger==True:
-                    best_stock.pop()
-                    continue
-                
-                
-                # save the maximum devidend as a global variable
-                if dev_tot >= max_dev:
-                    if dev_tot==max_dev:
-                        max_dev_hist.append(list(best_stock))
-#                        best_stock_history_tmp.append(list(best_stock_tmp))
+#            print("tmp",best_stock_tmp)
+#            print("stock", stock)
+#            print(start)
 
-                    elif dev_tot>max_dev:
-                        max_dev_hist=[]
-#                        best_stock_history_tmp=[]
-                        max_dev_hist.append(list(best_stock))
-#                        best_stock_history_tmp.append(list(best_stock_tmp))
-
-                    max_dev = dev_tot
-#                    print(max_dev)
-                # add the bought stock to the stack
-                best_stock_history.append(list(best_stock))
-                history.append(list(best_stock))
-                # the key attribute works as name info.
-                graph.add_node(dev_tot)
-                # add weighted edge from where you were to to stock you just bought
-                graph.add_weighted_edges_from(
-                    [(start, dev_tot, cost)], key=excell_data.iloc[stock, 0])
-                if bank_tmp >= cheapest_stock:
-                    # recursively call the function again, start in the just bought stock
-                    build_graph(dev_tot, bank_tmp, excell_data, graph, best_stock)
+            # save the maximum devidend as a global variable
+            if dev_tot >= max_dev:
+                flagsator==True
+#                    print('inside',dev_tot)
+                if dev_tot==max_dev:
+                    max_dev_hist.append(list(best_stock))
+#                    best_stock_history_tmp.append(list(best_stock_tmp))
+                elif dev_tot>max_dev:
+                    max_dev_hist=[]
+#                    best_stock_history_tmp=[]
+                    max_dev_hist.append(list(best_stock))
                     
-                    best_stock = best_stock_history.pop()
-                    del best_stock[-1:]
-                bank=bank_tmp
+#                    best_stock_history_tmp.append(list(best_stock_tmp))
                 
-                    
+#                print('dev_hist',max_dev_hist)
+#                print("tmp",best_stock_tmp)
+                max_dev = dev_tot
+            
+            # add the bought stock to the stack
+            best_stock_history.append(list(best_stock))
+            best_stock_history_tmp.append(list(best_stock_tmp))
+
+            history.append(list(best_stock))
+            # the key attribute works as name info.
+            graph.add_node(dev_tot)
+            # add weighted edge from where you were to to stock you just bought
+            graph.add_weighted_edges_from(
+                [(start, dev_tot, cost)], key=excell_data.iloc[stock, 0])
+            
+            if bank_tmp >= cheapest_stock:
+#                best_stock_local=best_stock
+#                best_stock_local_tmp=best_stock_tmp
+#                if len(list(filter(lambda i: i['name'] == excell_data.iloc[1, 0] , best_stock)))!=1 and len(list(filter(lambda i: i['name'] == excell_data.iloc[0, 0] , best_stock)))==1 and len(list(filter(lambda i: i['name'] == excell_data.iloc[2, 0] , best_stock)))==1:
+#                if flagsator==True:    
+#                print('PREV RECK',best_stock_tmp)
+                
+#                print(stock)
+#                    flagsator=True
+                # recursively call the function again, start in the just bought stock
+                build_graph(dev_tot, bank_tmp, excell_data, graph, best_stock,best_stock_tmp)
+
+##                if len(list(filter(lambda i: i['name'] == excell_data.iloc[1, 0] , best_stock)))!=1 and len(list(filter(lambda i: i['name'] == excell_data.iloc[0, 0] , best_stock)))==1 and len(list(filter(lambda i: i['name'] == excell_data.iloc[2, 0] , best_stock)))==1:
+#                if flagsator==True:
+#                print('After RECK',best_stock_tmp)
+##                    print('After RECK local',best_stock_local_tmp)
+#                    tmp=best_stock_history.pop()
+#                    print('POOP',tmp)
+#                    best_stock_history.append(list(tmp))
+#                    print(stock)
+#                best_stock=best_stock_local
+#                best_stock_tmp=best_stock_local_tmp
+            if len(list(filter(None,best_stock)))!=0:
+                best_stock=best_stock_history.pop()
+                best_stock_tmp=best_stock_history_tmp.pop()
+                
+#            if len(list(filter(lambda i: i['name'] == excell_data.iloc[1, 0] , best_stock)))!=1 and len(list(filter(lambda i: i['name'] == excell_data.iloc[0, 0] , best_stock)))==1 and len(list(filter(lambda i: i['name'] == excell_data.iloc[2, 0] , best_stock)))==1:
+#            if flagsator==True:
+#            print('PREV del',best_stock_tmp)
+#            print(stock)
+
+#                best_stock = best_stock_history.pop()
+#                if len(list(filter(None,best_stock)))>=2:
+#                    best_stock=[i for i in best_stock if not (i['index']==stock)]
+            
+            if len(list(filter(None,best_stock)))!=0:
+                #de här tar bort all occurencys and not the first
+#                best_stock=[i for i in best_stock if not (i['index']==stock) and flagsator==False]
+                
+                
+                best_stock=del_first_occurence(best_stock,stock)
+                best_stock_tmp=del_first_occurence(best_stock_tmp,stock)
+          
+#                best_stock_tmp=[i for i in best_stock_tmp if not (i['index']==stock)]
+                
+#            if len(list(filter(lambda i: i['name'] == excell_data.iloc[1, 0] , best_stock)))!=1 and len(list(filter(lambda i: i['name'] == excell_data.iloc[0, 0] , best_stock)))==1 and len(list(filter(lambda i: i['name'] == excell_data.iloc[2, 0] , best_stock)))==1:
+#            if flagsator==True:
+#            print("AFTER del",best_stock_tmp)
+#            print(stock)
+        #kolla om det finns ett bra sätt att börja om loopa från början om detta inträffar
+#        elif stock == max(excell_data.index) and bank>cheapest_stock:
+#            print('knas')
+#            flags=True
+#            magic_number=magic_number+1
+        
+
 
 
 def find_cheapest_edge(edge_data):
@@ -107,7 +174,6 @@ def find_cheapest_edge(edge_data):
 
 def gen_data_to_excell(stocks, excell_data, file_loc):
     writer = ExcelWriter(file_loc,engine='xlsxwriter')
-    print(stocks)
     for stock in excell_data.index:
         amount=len(list(filter(lambda i: i['name'] == excell_data.iloc[stock, 0] , stocks)))
         df1 = pd.DataFrame({excell_data.iloc[stock, 0]})
@@ -116,6 +182,18 @@ def gen_data_to_excell(stocks, excell_data, file_loc):
         df2.to_excel(writer, 'Result', startcol=1,startrow=stock,header=None, index=False)
     writer.save()
 
+def del_first_occurence(stock_list,index):
+    stock_del=[]
+    flagsator=False
+    for i in stock_list:
+        if i['index']==index and flagsator==False:
+            flagsator=True
+            continue
+        stock_del.append(i)
+    return stock_del   
+
+flagsator=False
+flags=False
 
 # import excell. Change if you move the file
 file_location = "D:\Stocks\Aktie_Utdelning.xlsx"
@@ -133,9 +211,10 @@ data_write = pd.read_excel(file_location_write,
                            sheet_name='Result', keep_default_na=False)
 # The amount of money i have to buy for
 #bank=4685 borde ge en dev på 178.95 typ
-bank = 4685*2
+bank = 4000
+bank_init=bank
 # initialize
-magic_number = 2
+magic_number = 1
 # first node dev is 0 since nothing has been bought, and maximum devdidend is now 0 since no devidend yet
 start = 0
 max_dev = 0
@@ -160,10 +239,10 @@ for stock in data.index:
 best_stock_history = []
 max_dev_hist=[]
 history=[]
-#best_stock_tmp=[]
-#best_stock_history_tmp=[]
+best_stock_tmp=[]
+best_stock_history_tmp=[]
 # generates all possibe combinations of the given stocks, bank and magical number
-build_graph(start, bank, data, graph, best_stock)
+build_graph(start, bank, data, graph, best_stock,best_stock_tmp)
 
 # draw graph
 #nx.draw(graph, with_labels=True)
